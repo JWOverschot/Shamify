92f0c4c3780f2d8132271090839431262e7409ea
JWOverschot <schoolvanjis@gmail.com>
2019-09-28 15:48:41 +0200

    added client-keys_sample file

---
+++ app/scripts/client-keys_sample.ts
@@ -0,0 +1,5 @@
+// Rename file to client-keys and add spotify client info found on https://developer.spotify.com/dashboard
+module.exports = {
+    id: '', // Your client id
+    secret: '' // Your secret
+}
